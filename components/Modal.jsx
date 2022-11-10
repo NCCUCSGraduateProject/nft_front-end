773a499d01e8387617fbb45edfe11dbe01dcb060
CTHua <illiew2470@gmail.com>
2022-11-10 16:27:00 +0800

    fix: nft name display

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -17,27 +17,29 @@ const BigNFTBlock = ({ uri, image_url, name }) => {
   };
 
   return (
-    <div
-      className="flex max-w:1080 w:90% max-h:1920 h:80% my:5 jc:center ai:center cursor:pointer position:rel flex:col"
-      key={uri}
-      onClick={(e) => {
-        clickHandler(uri, image_url);
-      }}
-    >
-      {isLoading && (
-        <div className="h:50 w:50">
-          <Spinner />
-        </div>
-      )}
-      <Image
-        src={image_url || ""}
-        alt={uri}
-        layout="fill"
-        objectFit="contain"
-        className={`r:10 ${isLoading ? "hide" : ""}`}
-        onLoadingComplete={() => setIsLoading(false)}
-      />
-      <div className="f:white">{name || "NoName"}</div>
+    <div className="flex flex:col max-w:1080 w:90% max-h:1920 h:90% jc:center ai:center ">
+      <div
+        className="flex w:90% h:80% m:5 jc:center ai:center cursor:pointer position:rel flex:col"
+        key={uri}
+        onClick={(e) => {
+          clickHandler(uri, image_url);
+        }}
+      >
+        {isLoading && (
+          <div className="h:50 w:50">
+            <Spinner />
+          </div>
+        )}
+        <Image
+          src={image_url || ""}
+          alt={uri}
+          layout="fill"
+          objectFit="contain"
+          className={`block r:10 ${isLoading ? "hide" : ""}`}
+          onLoadingComplete={() => setIsLoading(false)}
+        />
+      </div>
+      <div className="f:white t:center">{name || "NoName"}</div>
     </div>
   );
 };
@@ -58,9 +60,9 @@ const ChooseModal = ({ onClose }) => {
       className="abs w:100vw h:100vh top:0 bg:rgba(107,106,109,0.5) z:10 flex jc:center ai:center"
       onClick={onClose}
     >
-      <div className="w:80% h:80% bg:gray-10 opacity:100% z:20 r:50">
-        <div className="f:50 f:white lh:2 t:center ">Choose One</div>
-        <div className="flex jc:center ai:center h:80%">
+      <div className="w:80% h:80% bg:gray-10 opacity:100% z:20 r:50 flex jc:center ai:center flex:col">
+        <div className="f:50 f:white lh:2 t:center">Choose One</div>
+        <div className="flex jc:center ai:center h:80% w:90%">
           {!option && (
             <div className="h:50 w:50">
               <Spinner />
