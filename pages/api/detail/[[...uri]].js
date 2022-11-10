f14b57173d440d994048a6b6f93b08ee4849d6ec
CTHua <illiew2470@gmail.com>
2022-11-10 15:25:52 +0800

    fix: change mogo to config

--- pages/api/detail/[[...uri]].js
+++ pages/api/detail/[[...uri]].js
@@ -1,13 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { MongoClient } from "mongodb"
-const MONGO_URL = process.env.MONGODB_URL;
+import MongoApp from "../config/mongo"
 const MONGO_DBNAME = process.env.MONGODB_DBNAME;
 
 export default async function handler(req, res) {
     const { uri } = req.query
     if (req.method === "GET") {
         // Create a new MongoClient
-        const client = new MongoClient(MONGO_URL, { useNewUrlParser: true });
+        const client = MongoApp;
         const database = client.db(MONGO_DBNAME);
         const recData = database.collection("recData");
         const query = [{ "$match": { uri: uri.join("/") } }];
