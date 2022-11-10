d6c9410117fdcdc29978a712dedcbb3a6e651368
CTHua <illiew2470@gmail.com>
2022-11-10 16:02:46 +0800

    feat: add spinner

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -1,21 +1,8 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
-
-const ChooseModal = ({ isOpen, onClose }) => {
-  const [display, setDisplay] = useState(false);
-  const [option, setOption] = useState(null);
-  useEffect(() => {
-    setDisplay(false);
-    const init = async () => {
-      const res = await fetch("/api/list/random");
-      const data = await res.json();
-      setOption(data.slice(0, 2));
-      console.log(data.slice(0, 2));
-      setDisplay(true);
-    };
-    init();
-  }, [isOpen]);
-
+import Spinner from "./Spinner";
+const BigNFTBlock = ({ uri, image_url, name }) => {
+  const [isLoading, setIsLoading] = useState(true);
   const clickHandler = (uri, image_url) => {
     let uriList = window.localStorage.getItem("like");
     if (uriList) {
@@ -27,43 +14,66 @@ const ChooseModal = ({ isOpen, onClose }) => {
     }
     window.localStorage.setItem("like", JSON.stringify(uriList));
     window.dispatchEvent(new Event("storage"));
-    onClose();
   };
 
-  if (display && isOpen)
-    return (
-      <div
-        className="abs w:100vw h:100vh top:0 bg:rgba(107,106,109,0.5) z:10 flex jc:center ai:center"
-        onClick={onClose}
-      >
-        <div className="w:80% h:80% bg:gray-10 opacity:100% z:20 r:50">
-          <div className="f:50 f:white lh:2 t:center ">Choose One</div>
-          <div className="flex jc:center h:90%">
-            {option &&
-              option.map(({ image_url, name, uri }) => (
-                <div
-                  className="flex max-w:1080 w:90% max-h:1920 h:90% my:5 jc:center ai:center cursor:pointer position:rel"
-                  key={uri}
-                  onClick={(e) => {
-                    e.preventDefault();
-                    clickHandler(uri, image_url);
-                  }}
-                >
-                  <Image
-                    src={image_url || ""}
-                    alt={uri}
-                    layout="fill"
-                    objectFit="contain"
-                    className="r:50"
-                  />
-                  {name}
-                </div>
-              ))}
-          </div>
+  return (
+    <div
+      className="flex max-w:1080 w:90% max-h:1920 h:80% my:5 jc:center ai:center cursor:pointer position:rel flex:col"
+      key={uri}
+      onClick={(e) => {
+        clickHandler(uri, image_url);
+      }}
+    >
+      {isLoading && (
+        <div className="h:50 w:50">
+          <Spinner />
+        </div>
+      )}
+      <Image
+        src={image_url || ""}
+        alt={uri}
+        layout="fill"
+        objectFit="contain"
+        className={`r:10 ${isLoading ? "hide" : ""}`}
+        onLoadingComplete={() => setIsLoading(false)}
+      />
+      <div className="f:white">{name || "NoName"}</div>
+    </div>
+  );
+};
+
+const ChooseModal = ({ onClose }) => {
+  const [option, setOption] = useState(null);
+  useEffect(() => {
+    const init = async () => {
+      const res = await fetch("/api/list/random");
+      const data = await res.json();
+      setOption(data.slice(0, 2));
+    };
+    init();
+  }, []);
+
+  return (
+    <div
+      className="abs w:100vw h:100vh top:0 bg:rgba(107,106,109,0.5) z:10 flex jc:center ai:center"
+      onClick={onClose}
+    >
+      <div className="w:80% h:80% bg:gray-10 opacity:100% z:20 r:50">
+        <div className="f:50 f:white lh:2 t:center ">Choose One</div>
+        <div className="flex jc:center ai:center h:80%">
+          {!option && (
+            <div className="h:50 w:50">
+              <Spinner />
+            </div>
+          )}
+          {option &&
+            option.map(({ image_url, name, uri }) => (
+              <BigNFTBlock uri={uri} image_url={image_url} name={name} />
+            ))}
         </div>
       </div>
-    );
-  else return <></>;
+    </div>
+  );
 };
 
 export default ChooseModal;
--- components/NFTBlock.jsx
+++ components/NFTBlock.jsx
@@ -1,20 +1,29 @@
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import placeholder from "../public/placeholder.png";
+import Spinner from "./Spinner";
 const NFTBlock = (props) => {
   const { uri, imageUrl } = props;
+  const [isLoading, setIsLoading] = useState(true);
   return (
     <Link href={`/detail/${uri}`}>
       <div
         className="flex max-w:360 w:90% h:256 my:5 jc:center ai:center b:1|solid|gray r:10 cursor:pointer position:rel"
         key={uri}
       >
+        {isLoading && (
+          <div className="h:50 w:50">
+            <Spinner />
+          </div>
+        )}
         <Image
           src={imageUrl || placeholder}
           alt={uri}
           layout="fill"
           objectFit="contain"
-          className="r:10"
+          className={`r:10 ${isLoading ? "hide" : ""}`}
+          onLoadingComplete={() => setIsLoading(false)}
         />
       </div>
     </Link>
