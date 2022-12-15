65eccb98afae683459d16677f67fb90349278bb3
CTHua <illiew2470@gmail.com>
2022-12-15 15:20:46 +0800

    feat: add like button

--- components/NFTDetail.jsx
+++ components/NFTDetail.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 import NFTBlock from "./NFTBlock";
 function NFTDetail(props) {
-  const { image_url, description, name, i2iCv, itemCF } = props.data;
+  const { uri, image_url, description, name, i2iCv, itemCF } = props.data;
   const [recommend, setRecommend] = useState({});
   useEffect(() => {
     async function fetchData() {
@@ -37,6 +37,19 @@ function NFTDetail(props) {
     fetchData();
   }, [props.data]);
 
+  const add2Like = (uri, image_url) => {
+    let uriList = window.localStorage.getItem("like");
+    if (uriList) {
+      uriList = JSON.parse(uriList);
+      uriList = uriList.filter((item) => item.uri !== uri);
+      uriList.push({ uri, image_url: image_url });
+    } else {
+      uriList = [{ uri, image_url: image_url }];
+    }
+    window.localStorage.setItem("like", JSON.stringify(uriList));
+    window.dispatchEvent(new Event("storage"));
+  };
+
   return (
     <>
       <div className="my:10vh r:20 px:40 py:20 shadow:inset|9px|9px|18px|rgba(21,21,21,0.2),inset|9px|-9px|18px|rgba(21,21,21,0.2),inset|-9px|-9px|18px|rgba(83,83,83,0.9),inset|9px|9px|23px|rgba(21,21,21,0.9)">
@@ -59,7 +72,16 @@ function NFTDetail(props) {
             />
           </div>
         </div>
+        <div
+          className={`h:40 px:20 f:white f:20 bg:linear-gradient(135deg,#313131|0%,#373737|100%) flex m:0 jc:center ai:center r:10 cursor:pointer shadow:9px|9px|18px|rgba(21,21,21,0.2),9px|-9px|18px|rgba(21,21,21,0.2),-9px|-9px|18px|rgba(83,83,83,0.9),9px|9px|23px|rgba(21,21,21,0.9) shadow:inset|9px|9px|18px|rgba(21,21,21,0.2),inset|9px|-9px|18px|rgba(21,21,21,0.2),inset|-9px|-9px|18px|rgba(83,83,83,0.9),inset|9px|9px|23px|rgba(21,21,21,0.9):hover opacity:0.8:hover ~500ms|ease`}
+          onClick={(e) => {
+            add2Like(uri, image_url);
+          }}
+        >
+          Like
+        </div>
       </div>
+
       <div className="my:10vh r:20 px:40 py:20 shadow:inset|9px|9px|18px|rgba(21,21,21,0.2),inset|9px|-9px|18px|rgba(21,21,21,0.2),inset|-9px|-9px|18px|rgba(83,83,83,0.9),inset|9px|9px|23px|rgba(21,21,21,0.9)">
         <h1 className="f:white t:left lh:3">itemCF Top10</h1>
         <div className="flex jc:space-between ai:center h:200 my:20 w:full">
