ff4a78e30c107f5c5c541208ead359ba1da507e4
CTHua <illiew2470@gmail.com>
2022-12-15 15:15:29 +0800

    fix: reuse modal

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -2,24 +2,24 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import Spinner from "./Spinner";
 const BigNFTBlock = ({ uri, image_url, name }) => {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const clickHandler = (uri, image_url) => {
-    let uriList = window.localStorage.getItem("like");
+    let uriList = window.localStorage.getItem("choise");
     if (uriList) {
       uriList = JSON.parse(uriList);
       uriList = uriList.filter((item) => item.uri !== uri);
-      uriList.push({ uri, image_preview_url: image_url });
+      uriList.push({ uri, image_url: image_url });
     } else {
-      uriList = [{ uri, image_preview_url: image_url }];
+      uriList = [{ uri, image_url: image_url }];
     }
-    window.localStorage.setItem("like", JSON.stringify(uriList));
+    window.localStorage.setItem("choise", JSON.stringify(uriList));
     window.dispatchEvent(new Event("storage"));
   };
 
   return (
-    <div className="flex flex:col max-w:1080 w:90% max-h:1920 h:90% jc:center ai:center ">
+    <div className="flex flex:col max-w:1080 w:90% max-h:1920 h:90% jc:center ai:center">
       <div
-        className="flex w:90% h:80% m:5 jc:center ai:center cursor:pointer position:rel flex:col"
+        className={`p:10 w:90% h:90% aspect:1/1 bg:linear-gradient(135deg,#313131|0%,#373737|100%) flex m:0 jc:center ai:center r:10 cursor:pointer position:rel shadow:9px|9px|18px|rgba(21,21,21,0.2),9px|-9px|18px|rgba(21,21,21,0.2),-9px|-9px|18px|rgba(83,83,83,0.9),9px|9px|23px|rgba(21,21,21,0.9) shadow:inset|9px|9px|18px|rgba(21,21,21,0.2),inset|9px|-9px|18px|rgba(21,21,21,0.2),inset|-9px|-9px|18px|rgba(83,83,83,0.9),inset|9px|9px|23px|rgba(21,21,21,0.9):hover opacity:0.8:hover ~500ms|ease`}
         key={uri}
         onClick={(e) => {
           clickHandler(uri, image_url);
@@ -33,13 +33,13 @@ const BigNFTBlock = ({ uri, image_url, name }) => {
         <Image
           src={image_url || ""}
           alt={uri}
-          layout="fill"
-          objectFit="contain"
-          className={`block r:10 ${isLoading ? "hide" : ""}`}
-          onLoadingComplete={() => setIsLoading(false)}
+          fill
+          sizes="90%"
+          className={`r:10 ${isLoading ? "hide" : "block"} position:rel!`}
+          // onLoadingComplete={() => setIsLoading(false)}
         />
       </div>
-      <div className="f:white t:center">{name || "NoName"}</div>
+      <div className="f:white t:center lh:5">{name || "NoName"}</div>
     </div>
   );
 };
@@ -48,7 +48,7 @@ const ChooseModal = ({ onClose }) => {
   const [option, setOption] = useState(null);
   useEffect(() => {
     const init = async () => {
-      const res = await fetch("/api/list/random");
+      const res = await fetch("/api/list/random?number=2");
       const data = await res.json();
       setOption(data.slice(0, 2));
     };
@@ -60,7 +60,7 @@ const ChooseModal = ({ onClose }) => {
       className="abs w:100vw h:100vh top:0 bg:rgba(107,106,109,0.5) z:10 flex jc:center ai:center"
       onClick={onClose}
     >
-      <div className="w:80% h:80% bg:gray-10 opacity:100% z:20 r:50 flex jc:center ai:center flex:col">
+      <div className="w:80% h:80% bg:#343434 opacity:100% z:20 r:50 flex jc:center ai:center flex:col">
         <div className="f:50 f:white lh:2 t:center">Choose One</div>
         <div className="flex jc:center ai:center h:80% w:90%">
           {!option && (
@@ -70,7 +70,12 @@ const ChooseModal = ({ onClose }) => {
           )}
           {option &&
             option.map(({ image_url, name, uri }) => (
-              <BigNFTBlock key={uri} uri={uri} image_url={image_url} name={name}/>
+              <BigNFTBlock
+                key={uri}
+                uri={uri}
+                image_url={image_url}
+                name={name}
+              />
             ))}
         </div>
       </div>
