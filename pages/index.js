da89d8215629338ee74bd38db4a0374caf26a9ed
CTHua <illiew2470@gmail.com>
2022-12-15 15:21:30 +0800

    fix: clear useless in index js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
 import { useState } from 'react'
 import Head from 'next/head'
-import NFTPage from '../components/NFTPage'
-import NFTLike from '../components/NFTLike'
 import NFTRandom from '../components/NFTRandom'
-import ChooseModal from '../components/Modal'
 export default function Home() {
   const [showModal, setShowModal] = useState(false)
 
@@ -18,29 +15,10 @@ export default function Home() {
         <h1 className="f:60 f:white lh:1">
           NFTintrist
         </h1>
-        {/* <h1 className="f:40 f:white lh:1 t:left">
-          Liked Item
-        </h1>
-        <NFTLike />
-
-        <div>
-          <button className="h:30 bg:gray-8 b:1px f:white" onClick={() => setShowModal(true)}>Open Modal</button>
-          <button className="h:30 bg:gray-8 b:1px f:white" onClick={() => {
-            window.localStorage.removeItem("like")
-            window.dispatchEvent(new Event("storage"));
-          }}>Clear Like</button>
-        </div> 
-        <hr /> */}
         <div className="flex flex:col jc:center ai:center">
           <NFTRandom />
         </div>
-        {/* <hr /> */}
-        {/* <h1 className="f:40 f:white lh:1 t:left">
-          List Explore
-        </h1>
-        <NFTPage /> */}
       </main>
-      {showModal && <ChooseModal onClose={() => { setShowModal(false) }} />}
     </div>
   )
 }
