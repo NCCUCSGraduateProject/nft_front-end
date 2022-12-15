ea0981e7d4ea55aafe1068345ab07d837866e56c
CTHua <illiew2470@gmail.com>
2022-12-15 15:29:02 +0800

    feat: add router tab

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+import { useRouter } from 'next/router'
 import Head from 'next/head'
 import NFTRandom from '../components/NFTRandom'
 export default function Home() {
-  const [showModal, setShowModal] = useState(false)
 
+  const router = useRouter()
   return (
     <div className="flex jc:center ai:center">
       <Head>
@@ -15,6 +15,32 @@ export default function Home() {
         <h1 className="f:60 f:white lh:1">
           NFTintrist
         </h1>
+        <div className="flex flex:row jc:center gap:20">
+          <div
+            className={`w:200 h:40 px:20 f:white f:20 bg:linear-gradient(135deg,#313131|0%,#373737|100%) flex m:0 jc:center ai:center r:10 cursor:pointer shadow:9px|9px|18px|rgba(21,21,21,0.2),9px|-9px|18px|rgba(21,21,21,0.2),-9px|-9px|18px|rgba(83,83,83,0.9),9px|9px|23px|rgba(21,21,21,0.9) shadow:inset|9px|9px|18px|rgba(21,21,21,0.2),inset|9px|-9px|18px|rgba(21,21,21,0.2),inset|-9px|-9px|18px|rgba(83,83,83,0.9),inset|9px|9px|23px|rgba(21,21,21,0.9):hover opacity:0.8:hover ~500ms|ease`}
+            onClick={() => {
+              router.push("/likedItem");
+            }}
+          >
+            Liked Item
+          </div>
+          <div
+            className={`w:200 h:40 px:20 f:white f:20 bg:linear-gradient(135deg,#313131|0%,#373737|100%) flex m:0 jc:center ai:center r:10 cursor:pointer shadow:9px|9px|18px|rgba(21,21,21,0.2),9px|-9px|18px|rgba(21,21,21,0.2),-9px|-9px|18px|rgba(83,83,83,0.9),9px|9px|23px|rgba(21,21,21,0.9) shadow:inset|9px|9px|18px|rgba(21,21,21,0.2),inset|9px|-9px|18px|rgba(21,21,21,0.2),inset|-9px|-9px|18px|rgba(83,83,83,0.9),inset|9px|9px|23px|rgba(21,21,21,0.9):hover opacity:0.8:hover ~500ms|ease`}
+            onClick={() => {
+              router.push("/choise");
+            }}
+          >
+            Choise Item
+          </div>
+          <div
+            className={`w:200 h:40 px:20 f:white f:20 bg:linear-gradient(135deg,#313131|0%,#373737|100%) flex m:0 jc:center ai:center r:10 cursor:pointer shadow:9px|9px|18px|rgba(21,21,21,0.2),9px|-9px|18px|rgba(21,21,21,0.2),-9px|-9px|18px|rgba(83,83,83,0.9),9px|9px|23px|rgba(21,21,21,0.9) shadow:inset|9px|9px|18px|rgba(21,21,21,0.2),inset|9px|-9px|18px|rgba(21,21,21,0.2),inset|-9px|-9px|18px|rgba(83,83,83,0.9),inset|9px|9px|23px|rgba(21,21,21,0.9):hover opacity:0.8:hover ~500ms|ease`}
+            onClick={() => {
+              router.push("/u2i");
+            }}
+          >
+            U2I Recommendation
+          </div>
+        </div>
         <div className="flex flex:col jc:center ai:center">
           <NFTRandom />
         </div>
