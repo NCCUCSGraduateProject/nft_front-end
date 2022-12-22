c86017fb111e5db7cdd1bf8e9bb641fbe07faa13
CTHua <illiew2470@gmail.com>
2022-12-22 14:18:13 +0800

    feat: add toast

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
 import '@master/css'
 import '@master/keyframes.css';
-
+import { Toaster } from "react-hot-toast";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Toaster />
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
