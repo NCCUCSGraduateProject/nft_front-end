b11129b53a06a8942ade3d7bc22bd1e84f06eb23
CTHua <illiew2470@gmail.com>
2022-12-26 00:34:54 +0800

    fix: video display

--- components/NFTBlock.jsx
+++ components/NFTBlock.jsx
@@ -7,6 +7,9 @@ const NFTBlock = (props) => {
   const { uri, imageUrl, size, scale } = props;
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
+  const isVideo = ["webm", "mp4", "mov"].includes(
+    imageUrl?.split(".")[imageUrl?.split(".").length - 1]
+  );
   return (
     <div
       // className={`p:10 w:${size} aspect:1/1 bg:linear-gradient(135deg,#313131|0%,#373737|100%) flex m:0 jc:center ai:center r:10 cursor:pointer position:rel shadow:9px|9px|18px|rgba(21,21,21,0.2),9px|-9px|18px|rgba(21,21,21,0.2),-9px|-9px|18px|rgba(83,83,83,0.9),9px|9px|23px|rgba(21,21,21,0.9) x:-5:hover y:-10:hover opacity:0.8:hover ~500ms|linear`}
@@ -21,14 +24,26 @@ const NFTBlock = (props) => {
           <Spinner />
         </div>
       )}
-      <Image
-        src={imageUrl || placeholder}
-        alt={uri}
-        fill
-        sizes="90%"
-        className={`r:10 ${isLoading ? "hide" : "block"} position:rel!`}
-        onLoadingComplete={() => setIsLoading(false)}
-      />
+      {isVideo ? (
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          src={imageUrl}
+          className={`r:10 ${isLoading ? "hide" : "block"} position:rel! w:90%`}
+          onLoadedData={() => setIsLoading(false)}
+        />
+      ) : (
+        <Image
+          src={imageUrl || placeholder}
+          alt={uri}
+          fill
+          sizes="90%"
+          className={`r:10 ${isLoading ? "hide" : "block"} position:rel!`}
+          onLoadingComplete={() => setIsLoading(false)}
+        />
+      )}
     </div>
   );
 };
